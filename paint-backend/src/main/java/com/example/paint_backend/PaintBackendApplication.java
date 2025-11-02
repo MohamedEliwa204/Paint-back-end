20ecacda52078fe2646c42df4111a9e32ddfe0d4
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-11-02 15:08:34 +0200

    first commit

---
+++ paint-backend/src/main/java/com/example/paint_backend/PaintBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.paint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintBackendApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaintBackendApplication.class, args);
+    }
+
+}
---
+++ paint-backend/src/test/java/com/example/paint_backend/PaintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paint_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
